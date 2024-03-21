d151c27c28542a7c25ec1e61c218846a53a59e54
Alessandro Chinchilla <ale030401@gmail.com>
2024-03-21 12:29:51 -0600

    Add: row and coll index on each cell + event listener

--- src/modules/dom.js
+++ src/modules/dom.js
@@ -3,24 +3,33 @@ const Dom = (function () {
     cell.classList.add("ship");
   }
 
-  function renderBoard(gameBoard, containerId) {
+  function renderBoard(gameBoard, containerId, handleClick) {
     const container = document.getElementById(containerId);
     container.innerHTML = "";
 
     const board = gameBoard.getBoard();
 
-    const missedShots = gameBoard.allShots;
-    const allShots = gameBoard.missedShots;
-
-    board.forEach((row) => {
-      row.forEach((cell) => {
+    board.forEach((row, rowIndex) => {
+      row.forEach((cell, colIndex) => {
         const cellElement = document.createElement("div");
-
         cellElement.className = "cell";
+        cellElement.dataset.row = rowIndex; // Attach row and column index as data attributes
+        cellElement.dataset.col = colIndex;
+
+        if (cell === false) {
+          cellElement.classList.add("miss");
+        }
 
         if (cell !== null) {
           renderBoat(cellElement);
         }
+
+        cellElement.addEventListener("click", () => {
+          console.log(
+            `Cell at row ${rowIndex}, column ${colIndex} was clicked.`,
+            typeof cell === "object",
+          );
+        });
 
         container.appendChild(cellElement);
       });
