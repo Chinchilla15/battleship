8d5b6ff9fe24443d8553a7a49715c947f8188f4a
Alessandro Chinchilla <ale030401@gmail.com>
2024-03-21 12:31:13 -0600

    Refactor functions to use existing methods from game board and player

--- src/modules/game.js
+++ src/modules/game.js
@@ -1,33 +1,88 @@
 import Ship from "./ship";
 import GameBoard from "./gameboard";
 import Players from "./player";
+import { Dom } from "./dom";
 
-function initializeGame() {
+export default function Game() {
   const playerOneBoard = GameBoard();
   playerOneBoard.initializeBoard();
 
   const playerTwoBoard = GameBoard();
   playerTwoBoard.initializeBoard();
 
-  const playerOne = Players(playerOneBoard);
-  const playerTwo = Players(playerTwoBoard);
+  const playerOne = Players(playerTwoBoard);
+  const playerTwo = Players(playerOneBoard);
 
-  playerOneBoard.placeShip(4, 4, 1, "vertical");
-  playerOneBoard.placeShip(8, 8, 1, "vertical");
-  playerOneBoard.placeShip(6, 1, 2, "horizontal");
-  playerOneBoard.placeShip(0, 0, 3, "vertical");
-  playerOneBoard.placeShip(0, 9, 3, "vertical");
-  playerOneBoard.placeShip(5, 5, 4, "horizontal");
-  playerOneBoard.placeShip(8, 1, 5, "horizontal");
+  let activePlayer = playerOne.playersInfo[0];
 
-  playerTwoBoard.placeShip(2, 3, 3, "vertical");
-
-  return {
-    playerOneBoard,
-    playerTwoBoard,
-    playerOne,
-    playerTwo,
+  const switchPlayer = () => {
+    activePlayer =
+      activePlayer === playerOne.playersInfo[0]
+        ? playerTwo.playersInfo[1]
+        : playerOne.playersInfo[0];
   };
-}
 
-export { initializeGame };
+  const restartPlayer = () => {
+    activePlayer =
+      activePlayer === playerTwo.playersInfo[1]
+        ? playerOne.playersInfo[0]
+        : playerOne.playersInfo[0];
+  };
+
+  function initializeGame() {
+    playerOneBoard.placeShip(8, 1, 5, "horizontal");
+
+    playerTwoBoard.placeShip(2, 4, 5, "vertical");
+  }
+
+  function renderBoards() {
+    Dom.renderBoard(playerOneBoard, "player-one-container");
+    Dom.renderBoard(playerTwoBoard, "player-two-container");
+  }
+
+  function startGame() {
+    initializeGame();
+    renderBoards();
+  }
+
+  function attackPlayerOneBoard() {
+    playerTwo.computerAttack();
+  }
+
+  function attackPlayerTwoBoard(row, col) {
+    playerOne.playerAttack(row, col);
+  }
+
+  function handleAttack(row, col) {
+    const currentPlayer = playerOne.playersInfo[0].turn ? playerOne : playerTwo;
+
+    console.log(currentPlayer.playersInfo[0].isAI);
+  }
+  /*
+  function switchPlayer() {
+    playerOne.turn = !playerOne.turn;
+    playerTwo.turn = !playerTwo.turn;
+  }
+*/
+  attackPlayerTwoBoard(0, 1);
+  attackPlayerOneBoard();
+
+  playerTwoBoard.receiveAttack(0, 0);
+  playerOneBoard.receiveAttack(0, 0);
+  /*
+  // Example attacks to test functionality
+  attackPlayerOneBoard(0, 0);
+  attackPlayerOneBoard(0, 1);
+  attackPlayerOneBoard(0, 2);
+  attackPlayerOneBoard(1, 0);
+  attackPlayerOneBoard(2, 0);
+  attackPlayerTwoBoard(1, 1);
+  attackPlayerTwoBoard(5, 6);
+  attackPlayerTwoBoard(1, 0);
+*/
+
+  console.log(playerOne.playersInfo[0].isAI);
+
+  handleAttack();
+  return { initializeGame, startGame, playerOneBoard, playerTwoBoard };
+}
