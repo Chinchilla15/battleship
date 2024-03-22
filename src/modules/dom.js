cfb84ccdd48c2b9220b1efa4ad4883faefefebd2
Alessandro Chinchilla <ale030401@gmail.com>
2024-03-21 18:10:10 -0600

    Add: function to update board, Update board render function to update board accordingly

--- src/modules/dom.js
+++ src/modules/dom.js
@@ -3,7 +3,15 @@ const Dom = (function () {
     cell.classList.add("ship");
   }
 
-  function renderBoard(gameBoard, containerId, handleClick) {
+  function showAttack(cell) {
+    cell.classList.add("hit");
+  }
+
+  function showMiss(cell) {
+    cell.classList.add("miss");
+  }
+
+  function renderBoard(gameBoard, containerId) {
     const container = document.getElementById(containerId);
     container.innerHTML = "";
 
@@ -13,22 +21,19 @@ const Dom = (function () {
       row.forEach((cell, colIndex) => {
         const cellElement = document.createElement("div");
         cellElement.className = "cell";
-        cellElement.dataset.row = rowIndex; // Attach row and column index as data attributes
+        cellElement.dataset.row = rowIndex;
         cellElement.dataset.col = colIndex;
 
-        if (cell === false) {
-          cellElement.classList.add("miss");
-        }
-
-        if (cell !== null) {
-          renderBoat(cellElement);
-        }
+        if (cell) renderBoat(cellElement);
 
         cellElement.addEventListener("click", () => {
-          console.log(
-            `Cell at row ${rowIndex}, column ${colIndex} was clicked.`,
-            typeof cell === "object",
-          );
+          const rowData = cellElement.dataset.row;
+          const colData = cellElement.dataset.col;
+
+          gameBoard.receiveAttack(rowData, colData);
+          console.log(board);
+
+          updateBoard(gameBoard, containerId, rowData, colData);
         });
 
         container.appendChild(cellElement);
@@ -36,8 +41,42 @@ const Dom = (function () {
     });
   }
 
-  function showAttack(cell) {
-    cell.classList.add("hit");
+  function updateBoard(gameBoard, containerId, clickedRow, clickedCol) {
+    const container = document.getElementById(containerId);
+    const cells = container.querySelectorAll(".cell");
+    let shipSunk = null; // Variable to keep track if a ship has been sunk
+
+    cells.forEach((cell) => {
+      const rowIndex = cell.dataset.row;
+      const colIndex = cell.dataset.col;
+      const cellData = gameBoard.getBoard()[rowIndex][colIndex];
+
+      if (cellData === false) {
+        showMiss(cell);
+      } else if (
+        cellData &&
+        rowIndex === clickedRow &&
+        colIndex === clickedCol
+      ) {
+        showAttack(cell);
+        if (cellData.isSunk()) {
+          shipSunk = cellData; // Mark that a ship has been sunk
+        }
+      }
+    });
+
+    // Find the sunk ship and add the sunk class
+    if (shipSunk !== null) {
+      cells.forEach((cell) => {
+        const rowIndex = cell.dataset.row;
+        const colIndex = cell.dataset.col;
+        const cellData = gameBoard.getBoard()[rowIndex][colIndex];
+
+        if (cellData === shipSunk) {
+          cell.classList.add("sunk");
+        }
+      });
+    }
   }
 
   function disableBoard() {}
@@ -46,8 +85,7 @@ const Dom = (function () {
 
   return {
     renderBoard,
-    renderBoat,
-    showAttack,
+    updateBoard,
     disableBoard,
     addShip,
   };
