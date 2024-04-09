d817f95ca935c2c3410f45200ad0e3f08f739fe2
Alessandro Chinchilla <ale030401@gmail.com>
2024-04-09 11:56:28 -0600

    Update game logic

--- src/modules/player.js
+++ src/modules/player.js
@@ -1,18 +1,13 @@
 export default function Players(gameBoard) {
-  const { allShots } = gameBoard;
-  const { missedShots } = gameBoard;
-
   const playersInfo = [
     {
       name: "Player one",
       isAI: false,
-      turn: true,
       win: false,
     },
     {
       name: "Player two",
       isAI: true,
-      turn: false,
       win: false,
     },
   ];
@@ -24,11 +19,13 @@ export default function Players(gameBoard) {
     do {
       row = Math.floor(Math.random() * 10);
       col = Math.floor(Math.random() * 10);
+    } while (
       // eslint-disable-next-line no-loop-func
-    } while (allShots.some((shot) => shot.row === row && shot.col === col)); // Ensure AI does not attack the same cell twice and within bounds
+      gameBoard.allShots.some((shot) => shot.row === row && shot.col === col)
+    ); // Ensure AI does not attack the same cell twice and within bounds
 
     gameBoard.receiveAttack(row, col);
-    const coordinates = [row, col];
+    const coordinates = { row, col };
     return { coordinates };
   }
 
